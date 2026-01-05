007c299a1c94f92342f9567a069e2c0c61a01e24
efagbola <evelyn.fagbola@gmail.com>
2026-01-05 23:07:33 +0100

    Finalize run_all pipeline and run all estimation methods

--- code/run_all.py
+++ code/run_all.py
@@ -1,24 +1,15 @@
-from config import DATA_PATH
-from utils.price_curves import load_data, get_available_dates, build_price_curve
+"""
+Run all option-implied moment estimation methods.
+"""
 
-caps, floors, swaps = load_data(DATA_PATH)
+from method_main import run_method as run_nonparametric
+from method_parametric_normal import run_method as run_parametric_normal
+from method_parametric_lognormal import run_method as run_parametric_lognormal
+from method_direct_moments_bkm import run_method as run_direct_bkm
 
-dates = get_available_dates(caps, floors)
-print("Number of dates:", len(dates))
-
-example_date = dates[0]
-example_area = caps["area"].unique()[0]
-
-curve = build_price_curve(caps, floors, swaps, example_date, example_area)
-
-print("Example date:", example_date)
-print("Example area:", example_area)
-print("Strikes:", curve[0])
-print("Prices:", curve[1])
-
-from method_main import run_method as run_main
-from method_parametric_normal import run_method as run_param_normal
 
 if __name__ == "__main__":
-    run_main()
-    run_param_normal()
+    run_nonparametric()
+    run_parametric_normal()
+    run_parametric_lognormal()
+    run_direct_bkm()
