d14039df5a85500e0405a00349c8fc77f68fccdf
efagbola <evelyn.fagbola@gmail.com>
2026-01-08 21:03:30 +0100

    Add utils package init and update config paths

--- code/config.py
+++ code/config.py
@@ -1,4 +1,4 @@
 # Must be updated before running the code
-DATA_PATH = "/Users/evelyn/Desktop/M2/Quant methods/"
+DATA_PATH = "https://raw.githubusercontent.com/efagbola/option-prices-project/refs/heads/main/code/Data/"
 
 OUTPUT_PATH = "output/"
