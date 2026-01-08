257cdd872cd1758b1b3af648090210bf8017bd91
efagbola <evelyn.fagbola@gmail.com>
2026-01-08 21:21:22 +0100

    Create output directory automatically if missing

--- code/config.py
+++ code/config.py
@@ -1,4 +1,21 @@
-# Must be updated before running the code
-DATA_PATH = "https://raw.githubusercontent.com/efagbola/option-prices-project/refs/heads/main/code/Data/"
+import os
+from pathlib import Path
 
-OUTPUT_PATH = "output/"
+# Chemin des données
+data_path = "https://raw.githubusercontent.com/efagbola/option-prices-project/refs/heads/main/code/Data/"
+
+# Déterminer le chemin du dossier output
+def get_output_path():
+    # Chemin du dossier parent du dossier actuel
+    parent_dir = Path(__file__).parent.parent
+    output_dir = parent_dir / 'output'
+    
+    # Créer le dossier s'il n'existe pas
+    output_dir.mkdir(exist_ok=True)
+    
+    # Retourner le chemin absolu sous forme de chaîne avec un séparateur à la fin
+    return str(output_dir) + os.sep
+
+# Définir les chemins globaux
+DATA_PATH = data_path
+OUTPUT_PATH = get_output_path()
