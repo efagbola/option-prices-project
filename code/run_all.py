e3895e4c739eaf2ba010145d0212581124883ca3
Lorenzo <lorenzo.marasco147@gmail.com>
2026-01-10 14:15:32 +0100

    fixing issues and run plot on main

---
+++ code/run_all.py
@@ -0,0 +1,16 @@
+"""
+Run all option-implied moment estimation methods.
+"""
+
+from method_main import run_method as run_nonparametric
+from method_parametric_normal import run_method as run_parametric_normal
+from method_parametric_lognormal import run_method as run_parametric_lognormal
+from method_direct_moments_bkm import run_method as run_direct_bkm
+import plot_comparison
+
+if __name__ == "__main__":
+    run_nonparametric()
+    run_parametric_normal()
+    run_parametric_lognormal()
+    run_direct_bkm()
+    plot_comparison.run_plot()
