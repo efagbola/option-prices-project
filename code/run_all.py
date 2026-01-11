6c8b7154cd70aa967ce95d6b8f6135045b3e791a
Ethan Benchetrit <ethan.benchetrit@proton.me>
2026-01-11 17:10:40 +0100

    fix run_all and add outputs

--- code/run_all.py
+++ code/run_all.py
@@ -1,12 +1,14 @@
 """
 Run all option-implied moment estimation methods.
 """
+
+import plot_comparison
+from method_direct_moments_bkm import run_method as run_direct_bkm
+
 #
 from method_main import run_method as run_nonparametric
-from method_parametric_normal import run_method as run_parametric_normal
 from method_parametric_lognormal import run_method as run_parametric_lognormal
-from method_direct_moments_bkm import run_method as run_direct_bkm
-import plot_comparison
+from method_parametric_normal import run_method as run_parametric_normal
 
 if __name__ == "__main__":
     run_nonparametric()
@@ -14,3 +16,4 @@ if __name__ == "__main__":
     run_parametric_lognormal()
     run_direct_bkm()
     plot_comparison.run_plot()
+    import diagnose_divergences
